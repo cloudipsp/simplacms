5b98f44c67d3f300525fe0c92e1167a7a310b77f
dmiroshnikov <dmmatrix222@gmail.com>
2017-06-13 06:59:36 +0300

    host-to-host

--- payment/fondy/fondy.php
+++ payment/fondy/fondy.php
@@ -31,10 +31,11 @@ class Fondy extends Simpla
 		$currency = $payment_currency->code;
 		if ($currency == 'RUR')
 		  $currency = 'RUB';
-        if ($settings[lang]=='') {
-            $settings[lang] ='ru';
+        if ($settings['lang']=='') {
+            $settings['lang'] ='ru';
         }
-		$oplata_args = array('order_id' => $order_id . fondycsl::ORDER_SEPARATOR . time(),
+		$oplata_args = array(
+			'order_id' => $order_id . fondycsl::ORDER_SEPARATOR . time(),
 			'merchant_id' => $settings['fondy_merchantid'],
 			'order_desc' => $desc,
 			'amount' => $price,
@@ -45,102 +46,117 @@ class Fondy extends Simpla
 			'sender_email' => $order->email);
 
 		$oplata_args['signature'] = fondycsl::getSignature($oplata_args, $settings[fondy_secret]);
- $out = '<script src="https://api.fondy.eu/static_common/v1/checkout/ipsp.js"></script>
+		$url = $this->get_checkout($oplata_args);
+			$out = '<script src="https://api.fondy.eu/static_common/v1/checkout/ipsp.js"></script>
                     <script src="https://rawgit.com/dimsemenov/Magnific-Popup/master/dist/jquery.magnific-popup.js"></script>
                     <link href="https://rawgit.com/dimsemenov/Magnific-Popup/master/dist/magnific-popup.css" type="text/css" rel="stylesheet" media="screen">
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+			<div id="checkout">
+			<div id="checkout_wrapper">
+			</div>
+			</div>';
+			if ($settings['mode'] == 'popup'){
+			 $out .='<script>
+			function callmag(){
+			$.magnificPopup.open({
+			showCloseBtn:false,
+					items: {
+						src: $("#checkout_wrapper"),
+						type: "inline"
+					}
+				});
+			}
+			$(document).ready(function() {
+			 $.magnificPopup.open({
+			 showCloseBtn:false,
+					items: {
+						src: $("#checkout_wrapper"),
+						type: "inline"
+					}
+				});
+				})
+			</script>';
+				}
 
-
-<div id="checkout">
-<div id="checkout_wrapper">
-</div>
-</div>';
-if ($settings['mode'] == 'popup'){
- $out .='<script>
-function callmag(){
-$.magnificPopup.open({
-showCloseBtn:false,
-        items: {
-            src: $("#checkout_wrapper"),
-            type: "inline"
-        }
-    });
-}
-$(document).ready(function() {
- $.magnificPopup.open({
- showCloseBtn:false,
-        items: {
-            src: $("#checkout_wrapper"),
-            type: "inline"
-        }
-    });
-    })
-</script>';
+				 $out .='
+				 <style>
+			#checkout_wrapper a{
+				font-size: 20px;
+				top: 30px;
+				padding: 20px;
+				position: relative;
+			}
+			#checkout_wrapper {
+				text-align: left;
+				position: relative;
+				background: #FFF;
+				/* padding: 30px; */
+				padding-left: 15px;
+				padding-right: 15px;
+				padding-bottom: 30px;
+				width: auto;
+				max-width: 2000px;
+				margin: 9px auto;
+			}
+			</style>
+				 
+				 <script>
+			function checkoutInit(url, val) {
+				$ipsp("checkout").scope(function() {
+					this.setCheckoutWrapper("#checkout_wrapper");
+					this.addCallback(__DEFAULTCALLBACK__);
+					this.action("show", function(data) {
+					   $("#checkout_loader").remove();
+						$("#checkout").show();
+					});
+					this.action("hide", function(data) {
+						$("#checkout").hide();
+					});
+					if(val){
+					this.width(val);
+					this.action("resize", function(data) {
+					$("#checkout_wrapper").width(val).height(data.height);
+						});
+					}else{
+					 this.action("resize", function(data) {
+					$("#checkout_wrapper").width(480).height(data.height);
+						});
+					}
+					this.loadUrl(url);
+				});
+				};
+				checkoutInit("' . $url . '");
+				</script>';
+				if ($settings['mode'] == 'popup'){
+				 $out .='<input type="button" onclick="callmag();" class="checkout_button" value="'.$button_text.'">';
+				}
+				return $out;
 	}
-
-	 $out .='
-	 <style>
-#checkout_wrapper a{
-    font-size: 20px;
-    top: 30px;
-    padding: 20px;
-    position: relative;
-}
-#checkout_wrapper {
-    text-align: left;
-    position: relative;
-    background: #FFF;
-    /* padding: 30px; */
-    padding-left: 15px;
-    padding-right: 15px;
-    padding-bottom: 30px;
-    width: auto;
-    max-width: 2000px;
-    margin: 9px auto;
-}
-</style>
-	 
-	 <script>
-function checkoutInit(url, val) {
-	$ipsp("checkout").scope(function() {
-		this.setCheckoutWrapper("#checkout_wrapper");
-		this.addCallback(__DEFAULTCALLBACK__);
-		this.action("show", function(data) {
-           $("#checkout_loader").remove();
-            $("#checkout").show();
-        });
-		this.action("hide", function(data) {
-            $("#checkout").hide();
-        });
-        if(val){
-        this.width(val);
-        this.action("resize", function(data) {
-        $("#checkout_wrapper").width(val).height(data.height);
-            });
-        }else{
-         this.action("resize", function(data) {
-        $("#checkout_wrapper").width(480).height(data.height);
-            });
-        }
-		this.loadUrl(url);
-	});
-    };
-    var button = $ipsp.get("button");
-    button.setMerchantId('.$oplata_args[merchant_id].');
-    button.setAmount('.$oplata_args[amount].', "'.$oplata_args[currency].'", true);
-    button.setHost("api.fondy.eu");
-    button.addParam("order_desc","'.$oplata_args[order_desc].'");
-    button.addParam("order_id","'.$oplata_args[order_id].'");
-	button.addParam("signature","' . $oplata_args[signature] . '");
-    button.addParam("lang","'.$oplata_args[lang].'");//button.addParam("delayed","N");
-    button.addParam("server_callback_url","'.$oplata_args[server_callback_url].'");
-    button.addParam("sender_email","'.$oplata_args[sender_email].'");
-    button.setResponseUrl("'.$oplata_args[response_url].'");
-    checkoutInit(button.getUrl());
-    </script>';
-	if ($settings['mode'] == 'popup'){
-     $out .='<input type="button" onclick="callmag();" class="checkout_button" value="'.$button_text.'">';
-	}
-	return $out;
+	protected function get_checkout($args){
+		if(is_callable('curl_init')){
+		$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'https://api.fondy.eu/api/checkout/url/');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('request'=>$args)));
+			
+			$result = json_decode(curl_exec($ch));
+			$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+				
+			if ( $httpCode != 200 ){
+				echo "Return code is {$httpCode} \n"
+					.curl_error($ch);
+					exit;
+			} 
+			if ($result->response->response_status == 'failure'){
+				echo $result->response->error_message;
+				exit;
+			}
+			$url = $result->response->checkout_url;
+			return $url;
+		}else{
+			echo "Curl not found!";
+			die;
+		}			
 	}
 }
